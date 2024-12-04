5c04c4e74a99bc4366eafa35f0a17f23e4909434
0fbcb238c0 <private.paul.public@gmail.com>
2024-12-04 02:14:29 +0100

    Update build script to new components and fix apk download

--- revanced_build.sh
+++ revanced_build.sh
@@ -3,17 +3,15 @@
 # Check Ratelimit
 rt=$(curl -s https://api.github.com/rate_limit | jq .rate.remaining)
 # Defining files
-ints_url=$(curl -s https://api.github.com/repos/ReVanced/revanced-integrations/releases/latest | jq -r .assets[0].browser_download_url)
 cli_url=$(curl -s https://api.github.com/repos/ReVanced/revanced-cli/releases/latest | jq -r .assets[0].browser_download_url)
 patches_json_url=$(curl -s https://api.github.com/repos/ReVanced/revanced-patches/releases/latest | jq -r .assets[0].browser_download_url)
-patches_jar_url=$(curl -s https://api.github.com/repos/ReVanced/revanced-patches/releases/latest | jq -r .assets[1].browser_download_url)
+patches_rvp_url=$(curl -s https://api.github.com/repos/ReVanced/revanced-patches/releases/latest | jq -r .assets[0].browser_download_url)
 
 if [ $rt -ge "8" ]
 then
     echo "Downloading files:"
-    wget -nc -q $ints_url
     wget -nc -q $cli_url
-    wget -nc -q $patches_jar_url
+    wget -nc -q $patches_rvp_url
     echo "Done!"
 else
     echo "API limit exceeded, wait for a few minutes"
@@ -21,9 +19,8 @@ else
 fi
 
 # Defining Versions
-ints=$(curl -s https://api.github.com/repos/ReVanced/revanced-integrations/releases/latest | jq -r .assets[0].name)
 cli=$(curl -s https://api.github.com/repos/ReVanced/revanced-cli/releases/latest | jq -r .assets[0].name)
-patches=$(curl -s https://api.github.com/repos/ReVanced/revanced-patches/releases/latest | jq -r .assets[1].name)
+patches=$(curl -s https://api.github.com/repos/ReVanced/revanced-patches/releases/latest | jq -r .assets[0].name)
 yt_vers=$(java -jar $cli list-versions $patches -f com.google.android.youtube | tr "\tb" "\n" | tr " " "\n" | grep ^[0-9] | sort | tail -1)
 web_vers=$(echo $yt_vers | tr "." "-")
 
@@ -31,8 +28,8 @@ web_vers=$(echo $yt_vers | tr "." "-")
 if [ ! -e com.google.youtube.com_$yt_vers.apk ]
 then
     echo -e "Recommended Youtube APK for this Release: \033[32;1m$yt_vers\033[0m"
-    link1=$(curl --user-agent Firefox -sL "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$web_vers-release/youtube-$web_vers-android-apk-download/" | grep forcebaseapk | cut -d \" -f 6)
-    link2=$(curl --user-agent Firefox -sL "https://www.apkmirror.com$link1" | grep download.php? | cut -d \" -f 12 | sed 's/amp;//')
+    link1=$(curl --user-agent Firefox -sL "https://www.apkmirror.com/apk/google-inc/youtube/youtube-$web_vers-release/youtube-$web_vers-2-android-apk-download/" | grep forcebaseapk | cut -d \" -f 6)
+    link2=$(curl --user-agent Firefox -sL "https://www.apkmirror.com$link1" | grep download.php? | cut -d \" -f 14)
 echo Download Youtube APK Version $yt_vers
     wget -nc --user-agent Firefox "https://www.apkmirror.com$link2" -O com.google.youtube.com_$yt_vers.apk
 fi
@@ -47,9 +44,7 @@ else
     java -Djava.awt.headless=true \
     -jar $cli \
     patch com.google.youtube.com_$yt_vers.apk \
-    -b $patches \
-    -m $ints \
-    -i selected_patches.json \
+    -p $patches \
     -o revanced_$yt_vers.apk
     echo "Done!"
     exit 0
